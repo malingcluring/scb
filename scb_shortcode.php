34d313f0f377c3abf51a3aee6f1cf90dc98d7206
Maling Cluring <maling.maling.maling.maling@gmail.com>
2018-04-11 10:51:58 +0700

    update shortcode
    POST SHORTCODE

--- scb_shortcode.php
+++ scb_shortcode.php
@@ -1,51 +1,75 @@
 <?php
 
 // ---------------------------------------------------------------------------------------------------------------- POST
-add_shortcode( 'post', 'scb_post' );
-function scb_post() {
-	$args = array(
-		'post_type' 		=> 'post' ,
-		'orderby' 			=> 'date' ,
-		'order' 			=> 'DESC' ,
-		'posts_per_page' 	=> 6,
-		'cat'         		=> '3',
-		'paged' 			=> get_query_var( 'paged' ),
-		'post_parent' 		=> $parent
-	);
-
-	$string = '';
-	$query = new WP_Query( $args );
-	if( $query->have_posts() ){
-		while( $query->have_posts() ){
-			$query->the_post();
-			$string .= '<div>' . get_the_content() . '</div>';
+	add_shortcode('post', 'scb_post');
+	function scb_post($atts) {
+		$cat = $atts['cat'];
+		$args = array(
+			'post_type' 		=> 'post' ,
+			'orderby' 			=> 'date' ,
+			'order' 			=> 'DESC' ,
+			'posts_per_page' 	=> 6,
+			'cat'         		=> $cat,
+			'paged' 			=> get_query_var('paged'),
+			'post_parent' 		=> $parent
+		);
+	
+		$string = '';
+		$query = new WP_Query( $args );
+		if( $query->have_posts() ){
+			while( $query->have_posts() ){
+				$query->the_post();
+				$string .= '<div>' . get_the_content() . '</div>';
+			}
 		}
+		wp_reset_postdata();
+		return $string;
+	
 	}
-	wp_reset_postdata();
-	return $string;
-
-}
-
-
-
-
-// ---------------------------------------------------------------------------------------------------------------- CAROUSEL
-add_shortcode( 'carousel', 'scb_carousel' );
-function scb_carousel( $atts, $content = null ) {
-	if( isset( $GLOBALS[ 'count' ] ) )
-		$GLOBALS[ 'count' ]++;
-	else
-		$GLOBALS[ 'count' ] = 0;
-	$GLOBALS[ 'default_count' ] = 0;
-		
 	
+	$categories = get_categories( $args );
+	$arrcategories=array();
+	foreach ( $categories as $category ) :
+		$value = $category->term_id;
+		$option_label = $category->name;
+		//array_push($arrcategories, $value => $option_label );
+		$arrcategories[$value] = $option_label;
+	endforeach;
+	
+	shortcode_ui_register_for_shortcode(
+		'post',
+		array(
+			'label' => 'Choose Post Category',
+			'listItemImage' => 'dashicons-filter',
+			'attrs' => array(
+				array(
+					'label' => 'Post Category',
+					'attr' => 'cat',
+					'type' => 'select',
+					'options' => $arrcategories
+				),
+			),
+			'post_type' => array( 'post', 'page' ),
+		)		
+	);
+	
+
+
+
+
+// ---------------------------------------------------------------------------------------------------------------- BG-IMG
+add_shortcode('background', 'scb_background');
+function scb_background($atts, $content = null) {
 	$atts = shortcode_atts(array(
-		'id'	=> 'carousel_id',
-		'class' => '',
-		'url'	=> ''
-	), $atts );
-	
-	
+		'url' => '',
+		'id' => ''
+	), $atts, 'background');
+	$id = ($atts['id']) ? 'id="' .$atts['id']. '"' : '';
+	$url = $atts['url'];
+	$result = '<div ' .$id. ' class="bg-img-float">'
+				. '<img src="' .$url. '" class="img-float">'
+				. '</div>';
+	return $result;	
 }
 
 ?>
